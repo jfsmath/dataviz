d38473614637f42fafd3e36ec7e1a6dd04e70076
jfsmath <111916208+jfsmath@users.noreply.github.com>
2025-11-10 16:18:08 -0500

    shiny worksheet

---
+++ Labs/Shiny Module/20251111.R
@@ -0,0 +1,33 @@
+## Last time (20251106):
+
+# basic structure of shiny app
+# type "shinyapp" and pick the snippet to get started
+
+
+## Inside the UI
+# Input syntax: {method}Input("inputID", ...)
+# Output syntax: {method}Output("outputID", ...)
+
+
+
+## Example input functions
+# numericInput()
+
+
+## Inside the server
+# Calling the input: input$inputID
+# Calling the output: output$outputID
+# Render syntax: render{Type}({...})
+
+
+## List of render functions:
+# renderText <--> textOutput
+# renderPrint <--> verbatimTextOutput
+# renderTable <--> tableOutput
+# renderDataTable <--> dataTableOutput
+# renderPlot <--> plotOutput
+
+
+
+
+
